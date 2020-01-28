e192fbb1a5725387176d73f2c4bd046c8c369e20
raad1masum <piraadwan@gmail.com>
2020-01-27 20:56:11 -0800

    add send when say word

--- ban.py
+++ ban.py
@@ -1,10 +1,18 @@
 import discord
 from discord.ext import commands
 
-client = commands.Bot(command_prefix = '.')
+client = discord.Client()
 
 @client.event
 async def on_ready():
     print('Bot is ready.')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.lower() == "deffo":
+        await message.channel.send("STOP SAYING THAT!!!")
 
 client.run('key')
