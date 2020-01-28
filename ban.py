58481f0b8490c094f25bca5688cba8ae94141833
raad1masum <piraadwan@gmail.com>
2020-01-27 19:53:34 -0800

    fix typo

--- ban.py
+++ ban.py
@@ -5,6 +5,6 @@ client = commands.Bot(command_prefix = '.')
 
 @client.event
 async def on_ready():
-    print('Bot is read.')
+    print('Bot is ready.')
 
-client.run('NjcxNTMzNjgyMzYzODU4OTQ0.Xi-UkQ.t9IOYjgFepSkWq7Itu_F0kDOe-U')
+client.run('key')
